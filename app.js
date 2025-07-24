7ea81bc4cbf41413c565434f79a6abe5397e03c1
jib2004 <obasaajibola04@gmail.com>
2025-07-24 13:17:23 +0100

    Changed cors origin to *

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -38,7 +38,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
   }),
 )
