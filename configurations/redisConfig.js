b30a0c939d082a042b7b9fc75934cef38a1997d9
jib2004 <obasaajibola04@gmail.com>
2026-03-08 17:04:19 +0100

    Setup redis for the backend

--- configurations/redisConfig.js
+++ configurations/redisConfig.js
@@ -1,1 +1,14 @@
-import {createClient} from 'redis'
+import { createClient } from 'redis';
+
+export const client = createClient({
+    url: process.env.REDIS_URL
+});
+
+client.on('error', err => console.log('Redis Client Error', err));
+
+await client.connect();
+
+await client.set('foo', 'bar');
+const result = await client.get('foo');
+console.log(result)  // >>> bar
+
